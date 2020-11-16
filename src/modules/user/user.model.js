7a519c8e79b15bac4402f44ee98167c2e3cad07d
mikiteek <mikiteek@gmail.com>
2020-11-16 20:57:07 +0200

    Create user schema

---
+++ src/modules/user/user.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  favoriteMovies: {
+    type: Schema.Types.ObjectId,
+    ref: "Movie",
+  },
+  watchedMovies: {
+    type: Schema.Types.ObjectId,
+    ref: "Movie",
+  },
+  queueMovies: {
+    type: Schema.Types.ObjectId,
+    ref: "Movie",
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
